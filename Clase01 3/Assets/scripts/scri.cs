d1193c76dafffc986385b4ee8cc5c068ddb16ea8
BrayanGalvis <47949200+BrayanGalvis@users.noreply.github.com>
2019-05-16 12:30:04 -0500

    Helicoptero
    Está vivo  :D

--- Clase01 3/Assets/scripts/scri.cs
+++ Clase01 3/Assets/scripts/scri.cs
@@ -5,35 +5,35 @@ using UnityEngine;
 public class scri : MonoBehaviour {
 	
 	public float angle;
-	float smooth = 1000.0f;
-    float tiltAngle = 1000000.0f;
-	
-	// Use this for initialization
+	float smooth = 360.0f;
+    float tiltAngle = 10.0f;
+	float tiltAngle2 = 100.0f;
+
+
 	void Start () {
 	
 	angle = 0f;
 		
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		
-		/*
-		//ROTACION EN UN ANGULO DADO CON ANGULOS DE EULER
-		Quaternion target = Quaternion.Euler(angle, 0, 0);
-		gameObject.transform.GetChild(1).transform.rotation = target;
-		angle+=0.1f;*/
-		
-		//ROTACION CON CUATERNIONES Y SLERP
-		// Smoothly tilts a transform towards a target rotation.
-        //float tiltAroundZ = Input.GetAxis("Horizontal") * tiltAngle;
+
         float tiltAroundX = Input.GetAxis("Horizontal") * tiltAngle;
 
-        // Rotate the cube by converting the angles into a quaternion.
+		float tiltAroundY = Input.GetAxis("Horizontal") * tiltAngle2;
+
+		tiltAngle = tiltAngle+10000000;
+
+		tiltAngle2 = tiltAngle2+1000;
+
         Quaternion target = Quaternion.Euler(0, tiltAroundX, 0);
 
-        // Dampen towards the target rotation
-        gameObject.transform.GetChild(0).transform.GetChild(0).transform.rotation = Quaternion.Slerp(transform.rotation, target,  Time.deltaTime * smooth);
-		
+		Quaternion target2 = Quaternion.Euler( tiltAroundY, 0, 0);
+
+		gameObject.transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.rotation = Quaternion.Slerp(transform.rotation, target,  Time.deltaTime * smooth);
+
+		gameObject.transform.GetChild(0).transform.GetChild(1).transform.GetChild(0).transform.rotation = Quaternion.Slerp(transform.rotation, target2,  Time.deltaTime * smooth);
+
 	}
 }
